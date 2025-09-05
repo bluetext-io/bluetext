7a01c7c99983df64fe275a885e8f77fcc20bfa0c
André Eriksson <aeriksson@users.noreply.github.com>
2025-09-05 09:11:23 +0200

    remove project code

--- project/frontend/app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/_index.tsx"),
-  route("*", "routes/splat.tsx"),
-] satisfies RouteConfig;
